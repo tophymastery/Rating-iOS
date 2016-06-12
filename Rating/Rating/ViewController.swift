62b72171f38010d964f89eff2ab133e1bc0d5346
Akadet <akadet85@gmail.com>
2016-06-12 12:03:58 +0700

    First commit

---
+++ Rating/Rating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rating
+//
+//  Created by Akadet on 6/12/2559 BE.
+//  Copyright © 2559 Akadet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
